34615ef9cdbf628747d6860a94b650c3e342dc29
Quintinno Digital <jquintinno@gmail.com>
2023-08-25 08:16:39 -0300

    Feature: Implementar Tela de Autenticação (Redimensionamento Automático))

--- electron.js
+++ electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 
 let appWindow;
 
@@ -13,6 +13,16 @@ function createWindow() {
     });
 
     appWindow.loadFile('dist/sisgernubankautdkt/index.html');
+
+    setTimeout(() => {
+        const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+        const windowWidth = 600;
+        const windowHeight = 350;
+        const x = Math.floor((width - windowWidth) / 2);
+        const y = Math.floor((height - windowHeight) / 2);
+        appWindow.setSize(windowWidth, windowHeight);
+        appWindow.setPosition(x, y);
+    }, 6000);
 
     appWindow.on('closed', function () {
         appWindow = null
